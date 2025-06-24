984336bceb183ac2bf9c657d221edd15ac128367
Aman Roy <royaman56456@gmail.com>
2025-06-24 17:25:03 +0530

    Updating client code

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "http://localhost:5000";
+export const url = "https://buzz-chat-app-server.onrender.com";
 
 export const axiosInstance = axios.create({
     headers: {
